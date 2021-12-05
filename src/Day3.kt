59da3e19fb9b8d2bafb17d7078c2e67470d76a3d
Kuglll <mark.frelih@gmail.com>
2021-12-05 12:48:19 +0100

    Finish Day 3, Part 1

--- src/Day3.kt
+++ src/Day3.kt
@@ -2,8 +2,34 @@ import java.io.File
 
 fun main(){
 
+    // Part 1
     val input = File("src/Inputs/Day3.txt")
 
-    
+    val inputLineLength = input.readLines()[0].length
+
+    val listForEachDigitPosition = mutableListOf<MutableList<Char>>()
+
+    for(i in 0..inputLineLength - 1){
+        val listOfDigits = mutableListOf<Char>()
+        input.forEachLine { inputLine ->
+            listOfDigits.add(inputLine.get(i))
+        }
+        listForEachDigitPosition.add(listOfDigits)
+    }
+
+    var gammaRate = ""
+    for(list in listForEachDigitPosition){
+        val (one, zero) = list.partition { digit -> digit.compareTo('1') == 0 }
+        gammaRate += (if(one.size > zero.size) "1" else "0")
+    }
+
+    var epsilonRate = ""
+    gammaRate.forEach {
+        epsilonRate += if(it.compareTo('1') == 0) "0" else "1"
+    }
+
+    println("Power consumption of the submarine: ${gammaRate.toInt(2) * epsilonRate.toInt(2)}")
+
+    //Part 2
 
 }
