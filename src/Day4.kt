8c1b35b283e3070faaf2be1b2e67b6b8532d015b
Kuglll <mark.frelih@gmail.com>
2021-12-05 19:23:43 +0100

    Finish Day 4 part 1

---
+++ src/Day4.kt
@@ -0,0 +1,93 @@
+import java.io.File
+
+fun main() {
+
+    // Part 1
+    val input = File("src/Inputs/Day4.txt").readLines()
+
+    val gameboards = mutableListOf<GameBoard>()
+
+    val drawnNumbers = input[0]
+
+    var tempGameboard = GameBoard()
+    for (line in input.drop(2)) {
+        if (line == "") {
+            gameboards.add(tempGameboard)
+            tempGameboard = GameBoard()
+            continue
+        }
+        val row = line.replace("  ", " ").split(" ").map {
+            NumberOnBoard(it)
+        }
+        tempGameboard.numbers.add(row)
+    }
+
+    loop@ for (number in drawnNumbers.split(",")) {
+        for (gameboard in gameboards) {
+            gameboard.markHitNumbers(number)
+            if (gameboard.isBoardWinning()) {
+                println("Final score: ${gameboard.getSumOfAllUnmarkedNumbers() * number.toInt()}")
+                break@loop
+            }
+        }
+    }
+
+
+}
+
+class GameBoard(
+    val numbers: MutableList<List<NumberOnBoard>> = mutableListOf()
+) {
+
+    fun markHitNumbers(number: String) {
+        numbers.forEach { row ->
+            row.forEach { numberOnBoard ->
+                if (numberOnBoard.number.compareTo(number) == 0) {
+                    numberOnBoard.isMarked = true
+                }
+            }
+        }
+    }
+
+    fun isBoardWinning() = isAnyRowWinning() || isAnyColumnWinning()
+
+    private fun isAnyRowWinning(): Boolean {
+        numbers.forEach { row ->
+            if(row.count { it.isMarked } == row.size){
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun isAnyColumnWinning(): Boolean {
+        val columnStatuses = mutableListOf<Boolean>()
+        for(i in 0 until numbers.size){
+            columnStatuses.add(isColumnWinning(i))
+        }
+
+        return columnStatuses.any{
+            it
+        }
+    }
+
+    private fun isColumnWinning(index: Int): Boolean {
+        numbers.forEach { row ->
+            if(!row[index].isMarked){
+                return false
+            }
+        }
+        return true
+    }
+
+    fun getSumOfAllUnmarkedNumbers() =
+        numbers.sumOf { row ->
+            row.filter { !it.isMarked }.map { it.number.toInt() }.sum()
+        }
+
+}
+
+class NumberOnBoard(
+    val number: String,
+    var isMarked: Boolean = false
+)
